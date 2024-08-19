c6fddae61e75fa240eb99011b9364d1d00efbc75
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2024-08-19 16:14:07 +0700

    feat; add relationship bengkels to related Bengkel

--- app/Models/Bengkel.php
+++ app/Models/Bengkel.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Bengkel extends Model
 {
     use HasFactory;
 
     protected $fillable = ['nama', 'alamat', 'latitude', 'longitude'];
+
+    protected $table = 'bengkel';
+
+    // Relasi One to Many dengan Bengkel
+    public function bengkels(): HasMany
+    {
+        return $this->hasMany(Bengkel::class, 'id_bengkel');
+    }
 }
