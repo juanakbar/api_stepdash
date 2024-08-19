cc95f2190f3a34e32ca004d2aab9b51679a71ac0
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2024-08-19 17:27:31 +0700

    feat: add mass assignment Setting

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Setting extends Model
 {
     use HasFactory;
+
+    protected $fillable =
+    [
+        'app_name',
+        'created_by',
+        'minimum_fare',
+        'version'
+    ];
 }
